bf67acc56bf1d7b6279bf0691173c5fb3fd96bcb
WilliamDevin23 <wdevinsp@gmail.com>
2024-10-31 22:20:33 +0700

    Basic structure for the page

---
+++ app/what-we-do/consulting-service/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+
+const page = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <div className="h-screen bg-neutral-400">
+				</div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default page;
