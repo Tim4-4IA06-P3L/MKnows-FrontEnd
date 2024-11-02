090381eece558f3f0b95b9c9ff135aed254a68fc
afiifmaul <153207143+afiifmaul@users.noreply.github.com>
2024-11-02 20:17:28 +0700

    Add part of content from the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,41 @@ export default function Home() {
     <div className="min-h-screen">
       <Navbar />
       <main>
-        <section className="min-h-screen">
-					<CoverPage bgSrc="" coverTitle="" />
-				</section>
+        {/* Hero */}
+        <section className="pb-14">
+          <CoverPage bgSrc="" coverTitle="" />
+        </section>
+
+        {/* Teks */}
+        <section className="pb-20">
+          <div className="container mx-auto text-center">
+            <div className="flex justify-evenly mb-12">
+              <div className="text-center">
+                <h2 className="text-2xl font-bold">100+ Pelatihan</h2>
+                <p className="text-gray-600">Dari berbagai klien</p>
+              </div>
+
+              <div className="h-12 border-l border-gray-300"></div>
+
+              <div className="text-center">
+                <h2 className="text-2xl font-bold">10+ Instruktur</h2>
+                <p className="text-gray-600">Instruktur Ternama</p>
+              </div>
+
+              <div className="h-12 border-l border-gray-300"></div>
+
+              <div className="text-center">
+                <h2 className="text-2xl font-bold">200+ Klien</h2>
+                <p className="text-gray-600">Dari berbagai perusahaan</p>
+              </div>
+            </div>
+            <h2 className="text-3xl font-bold mt-24 mb-9">
+              Jelajahi Kategori Unggulan
+            </h2>
+          </div>
+        </section>
+
+        {/* Grid */}
       </main>
       <Footer />
     </div>
