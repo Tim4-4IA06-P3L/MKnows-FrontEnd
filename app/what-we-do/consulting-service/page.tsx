d70b0bd602779b493901cbaf805d085cf9b5384f
WilliamDevin23 <wdevinsp@gmail.com>
2024-11-02 14:41:36 +0700

    Fill the page content

--- app/what-we-do/consulting-service/page.tsx
+++ app/what-we-do/consulting-service/page.tsx
@@ -2,15 +2,125 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import CoverPage from "../../components/CoverPage";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <main>
-        <div className="min-h-screen">
+        <section>
 					<CoverPage bgSrc="/Consulting_Img.jpg" coverTitle="Consulting Service M-Knows Consulting" />
-				</div>
+				</section>
+				
+				<section className="w-full flex flex-col items-center p-20">
+					<strong className="text-2xl text-center mb-5">Kemitraan SDM dan Bisnis Masa Depan</strong>
+					<p className="text-justify last-center">
+						Mempunyai hubungan jangka panjang dengan perusahaan-perusahaan lokal dan internasional 
+						yang berbasis di Indonesia untuk mendukung pengembangan sumber daya manusia mereka serta 
+						strategi bisnis dan manajemen. Program pengembangan ini dirancang untuk meningkatkan 
+						keahlian spesifik bahwa perusahaan membutuhkan strategi untuk menghadapi masa depan.
+					</p>
+				</section>
+				
+				<section className="w-full flex flex-row p-20 space-x-10">
+					<div className="relative w-full">
+						<Image
+							src="/15.png"
+							fill={true}
+							style={{
+								objectFit: "contain",
+							}}
+							alt="15"
+						/>
+					</div>
+					
+					<div>
+						<strong className="text-2xl">Kami telah menjadi mitra terpercaya (lebih dari 15 tahun)</strong>
+						<p className="text-justify mt-5">
+							Bagi perusahaan nasional dan multinasional dalam mendukung pengembangan bisnis dan strategi 
+							manajemen dan kompetensi sumber daya manusia. Kebanggaan berasak dari hubungan jangka panjang 
+							yang saling menguntungkan dengan berbagai perusahaaan yang memiliki visi untuk sukses, dan 
+							komitmen untuk mencapai kesuksesan itu melalui pengembangan sumber daya manusia.
+						</p>
+					</div>
+				</section>
+				
+				<section className="w-full flex flex-row p-20 space-x-10">
+					<div>
+						<strong className="text-2xl">Pengembangan Sumber Daya Manusia</strong>
+						<p className="text-justify mt-5">
+							Merupakan proses yang berkesinambungan yang membutuhkan komitmen yang tinggi dan manajemen yang 
+							baik untuk menjadi efektif dalam segala hal. Program pengembangan berkaitan dengan tahap pengembangan 
+							perusahaan dan tujuan jangka pendek serta jangka panjang mereka, baik saat start up, dalam ekspansi, 
+							dalam masa transisi atau berbalik. Perusahaan harus merencanakan dan merancang program pembangunan 
+							dengan hati-hati sesuai dengan setiap lapisan manajemen seperti : MT (Management Trainee), Leadership 
+							Program & Consulting.
+						</p>
+					</div>
+					
+					<div className="relative w-full">
+						<Image
+							src="/Rocket.png"
+							fill={true}
+							style={{
+								objectFit: "contain",
+							}}
+							alt="15"
+						/>
+					</div>
+				</section>
+				
+				<section className="w-full flex flex-row p-20 space-x-10">
+					<div className="relative w-full">
+						<Image
+							src="/pelatihan-efektif.png"
+							fill={true}
+							style={{
+								objectFit: "contain",
+							}}
+							alt="15"
+						/>
+					</div>
+					
+					<div>
+						<strong className="text-2xl">Program Pelatihan yang Efektif</strong>
+						<p className="text-justify mt-5">
+							Meningkatkan kompetensi sumber daya manusia untuk melakukan lebih baik dan nilai tambah bagi perusahaan 
+							serta institusi mereka. Pelatihan diperlukan untuk berbagai tujuan, misalnya mengembangkan keterampilan 
+							dan pengetahuan para eksekutif dan staf khusus, mempersiapkan sumber daya manusia baru untuk suatu posisi 
+							pekerjaan atau regenerasi, maupun mempersiapkan pensiun. M-Knows Consulting mendukung perusahaan dan bekerja 
+							sinergis dengan pusat-pusat pelatihan dalam menilai kebutuhan pelatihan, pengembangan pelatihan customized 
+							program, melakukan dan mengevaluasi pelatihan bagi staf profesional dan eksekutif. Konsultan yang berpengalaman 
+							dan berdedikasi tinggi memampukan M-Knows Consulting dalam menyusun materi pelatihan, menyampaikan materi 
+							pelatihan, serta metode evaluasi dalam rangka memperoleh hasil pelatihan yang terbaik.
+						</p>
+					</div>
+				</section>
+				
+				<section className="relative w-full flex flex-col items-center mt-20">
+					<div className="relative w-full h-[300px]">
+						<Image 
+							src="/business-2.jpg"
+							alt="Kerja Sama"
+							fill={true}
+							style={{
+								objectFit: "cover",
+								objectPosition: "bottom"
+							}}
+						/>
+					</div>
+					
+					<div className="absolute w-1/2 -translate-y-1/2 p-6 bg-[#AECFDF]">
+						<strong className="text-bold text-xl">M-Knows Consulting telah memperoleh banyak kesempatan bekerja sama</strong>
+						<p className="text-justify last-center">
+							Dengan lembaga-lembaga swasta dan pemerintah pada proyek-proyek perumusan kebijakan, studi banding, serta undangan 
+							untuk menjadi pembicara tamu dalam berbagai kesempatan dan pertemuan atau seminar untuk berbagi dan memperkenalkan 
+							pandangan dan pengetahuan di bidang ekonomi, manajemen dan bisnis.
+						</p>
+					</div>
+				</section>
+				
       </main>
       <Footer />
     </div>
--- app/what-we-do/executive-coaching-and-mentoring/page.tsx
+++ app/what-we-do/executive-coaching-and-mentoring/page.tsx
@@ -2,15 +2,70 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Navbar from "../../components/Navbar";
 import CoverPage from "../../components/CoverPage";
+import OverlayImage from "../../components/OverlayImage";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <Navbar />
       <main>
-        <div className="min-h-screen">
-					<CoverPage bgSrc="/Consulting_Img.jpg" coverTitle="Executing Coaching & Business Mentoring" />
-				</div>
+        <section>
+					<CoverPage bgSrc="/coaching.jpg" coverTitle="Executive Coaching & Business Mentoring" />
+				</section>
+				
+				<section className="w-full flex flex-col items-center p-20">
+					<strong className="text-2xl text-center mb-5">Peningkatan Keterampilan Kerja untuk Tim Efektif</strong>
+					<p className="text-justify last-center">
+						Program ini bertujuan untuk membantu peserta dalam memperoleh pengetahuan dan keterampilan fungsional 
+						yang dapat mereka terapkan di tempat kerja, khususnya bagi Anda yang memiliki tim (rekan kerja).
+					</p>
+				</section>
+				
+				<section className="w-full flex flex-row p-20 space-x-10">
+					<OverlayImage bgColor="#1C75BC" />
+					
+					<div>
+						<strong className="text-2xl">Tujuan Pendampingan</strong>
+						<p className="text-justify mt-5">
+							Mengatasi semua kesulitan yang di hadapi bawahan dalam mengerjakan tugas atau performance yang tidak mencapai 
+							dengan ketentuan yang diperintahkan sehingga daripada itu meningkatkan keahlian tertentu di dalam pekerjaan 
+							yang kita kerjakan untuk memberikan kepercayaan yang lebih besar dalam pekerjaan yang kita lakukan saat ini.
+						</p>
+					</div>
+				</section>
+				
+				<section className="w-full flex flex-row p-20 justify-between">
+					<div>
+						<strong className="text-2xl">Topik yang Ditawarkan</strong>
+						<ol className="list-decimal list-inside mt-5">
+							<li>Executive Coaching</li>
+							<li>Leadership Coaching</li>
+							<li>Business Mentoring</li>
+						</ol>
+						<p className="text-justify mt-5">
+							Untuk mengembangkan kompetensi dan keterampilan peserta sehingga mampu menjadi sumber daya yang handal dalam 
+							pencapaian target perusahaan.
+						</p>
+					</div>
+					
+					<div className="relative w-3/5">
+						<Image
+							src="/Rocket.png"
+							fill={true}
+							style={{
+								objectFit: "contain",
+							}}
+							alt="15"
+						/>
+					</div>
+				</section>
+				
+				<section className="mt-10">
+					<div className="flex flex-col justify-center">
+						<strong className="text-5xl text-center">Our Clients</strong>
+					</div>
+				</section>
       </main>
       <Footer />
     </div>
