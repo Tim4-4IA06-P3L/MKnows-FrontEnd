73678a5580a92908e9de90d2c2c3bb61557db4e4
WilliamDevin23 <wdevinsp@gmail.com>
2024-11-04 14:14:51 +0700

    Modify the trapezoid slope

--- app/components/CoverPage.tsx
+++ app/components/CoverPage.tsx
@@ -5,14 +5,14 @@ const CoverPage = ({ bgSrc, coverTitle }) => {
 		<div className="relative w-full bg-right-top bg-contain bg-no-repeat z-0"
 			style = {{ backgroundImage: `url('${bgSrc}')`}}
 		>
-			<div className="flex w-3/5 justify-around items-center">
-				<div className="absolute text-6xl p-5">
+			<div className="flex w-[65%] justify-around items-center">
+				<div className="absolute text-lg md:text-4xl lg:text-6xl p-5 max-w-[60%]">
 					<strong>{coverTitle}</strong>
 				</div>
 				<svg xmlns="http://www.w3.org/2000/svg"
-					 viewBox="0 0 1050 500">
-					<polygon fill="#AECFDF" points="0,0 1050,0 1000,500 0,500" />
-					<polygon fill="#B3FF00" points="1020,0 1050,0 1000,500 970,500" />
+					 viewBox="0 0 1050 450">
+					<polygon fill="#AECFDF" points="0,0 1050,0 900,450 0,450" />
+					<polygon fill="#B3FF00" points="1020,0 1050,0 900,450 870,450" />
 				</svg>
 			</div>
 		</div>
