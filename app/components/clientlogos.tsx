09f1a1ffeb36cc064e840fadf6745291414b100e
Iftah <iftahmaulana7@gmail.com>
2024-11-09 17:54:32 +0700

    add clientlogos component

---
+++ app/components/clientlogos.tsx
@@ -0,0 +1,217 @@
+import React from "react";
+
+const ClientLogos = () => {
+  return (
+    <div className="w-full md:w-[90%] lg:w-[80%] overflow-hidden bg-white py-10 mx-auto">
+      <h2 className="text-3xl md:text-4xl font-semibold text-[#1E3A8A] text-center mb-8">
+        Our Clients
+      </h2>
+      <div className="relative w-full">
+        {/* First row of logos - moving left */}
+        <div className="flex animate-scrollLeft">
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/MKnows_Logo.png"
+              alt="Clodeo"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Bridgestone"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="SMI"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Tripa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Mandiri"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="BNI"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/MKnows_Logo.png"
+              alt="Clodeo"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Bridgestone"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="SMI"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Tripa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Mandiri"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="BNI"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+        </div>
+
+        {/* Second row of logos - moving right */}
+        <div className="flex animate-scrollRight mt-4 md:mt-8">
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/client-logos/csm.png"
+              alt="CSM"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/indokordsa.png"
+              alt="Indokordsa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/csul-finance.png"
+              alt="CSUL Finance"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/mkom.png"
+              alt="MKOM"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/orico.png"
+              alt="Orico"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/bank.png"
+              alt="Bank"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/client-logos/csm.png"
+              alt="CSM"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/indokordsa.png"
+              alt="Indokordsa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/csul-finance.png"
+              alt="CSUL Finance"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/mkom.png"
+              alt="MKOM"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/orico.png"
+              alt="Orico"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/client-logos/bank.png"
+              alt="Bank"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+        </div>
+        {/* First row of logos - moving left */}
+        <div className="flex animate-scrollLeft">
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/MKnows_Logo.png"
+              alt="Clodeo"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Bridgestone"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="SMI"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Tripa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Mandiri"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="BNI"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+          <div className="flex min-w-full justify-around items-center gap-4 md:gap-8">
+            <img
+              src="/MKnows_Logo.png"
+              alt="Clodeo"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Bridgestone"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="SMI"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Tripa"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="Mandiri"
+              className="h-8 md:h-12 object-contain"
+            />
+            <img
+              src="/MKnows_Logo.png"
+              alt="BNI"
+              className="h-8 md:h-12 object-contain"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ClientLogos;
