d71025938258ad48d83e67e63a932279afe80a49
WilliamDevin23 <wdevinsp@gmail.com>
2024-11-13 13:05:56 +0700

    Add import react

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -21,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins} antialiased`}
       >
         {children}
       </body>
