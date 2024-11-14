27b79086bc6c921afeccaf72a156f8fe88de39dd
afiifmaul <153207143+afiifmaul@users.noreply.github.com>
2024-11-14 14:30:35 +0700

    Remove the card component and changing the grid to flex for public learning page

--- app/what-we-do/training/public-learning/page.tsx
+++ app/what-we-do/training/public-learning/page.tsx
@@ -1,18 +1,30 @@
 import React from "react";
 import Navbar from "../../../components/Navbar";
 import Footer from "../../../components/Footer";
-import Card from "../../../components/Card";
 import Link from "next/link";
+import Image from "next/image";
 
+const videos = [
+  { title: "Sales & Marketing Series", src: "/sms.jpeg" },
+  { title: "Credit & Collection Series", src: "/ccs.jpeg" },
+  { title: "HC & Training Series", src: "/hts.jpeg" },
+  { title: "Finance Series", src: "/fs.jpeg" },
+  { title: "Office & Administration Series", src: "/oas.jpeg" },
+  { title: "Leadership & Managerial Series", src: "/lms.jpeg" },
+  { title: "Other Series", src: "/os.jpeg" },
+];
 const App = () => {
   return (
-    <div className="bg-white text-gray-800">
+    <>
       <Navbar />
+      {/* Title */}
       <main className="text-center pt-14">
         <div>
           <h1 className="text-3xl font-bold text-black">Public Webtraining</h1>
           <h2 className="text-2xl text-blue-500">2024</h2>
         </div>
+
+        {/* Button */}
         <div className="mt-4 text-left pl-24 pb-10">
           <Link href="/what-we-do/training/online-learning/formulir">
             <button className="px-6 py-2 bg-slate-100 border border-sky-600 text-sky-700 rounded-md hover:bg-gray-200">
@@ -22,39 +34,32 @@ const App = () => {
         </div>
 
         {/* Grid */}
-        <section className="">
-          <div className="container mx-auto sm:px-4 md:px-6 lg:px-10 text-left">
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-20 mx-10 pb-20">
-              <Card
-                title="Sales & Marketing Series"
-                imgSrc="/sms.jpeg"
-                link="#"
-              />
-              <Card
-                title="Credit & Collection Series"
-                imgSrc="/ccs.jpeg"
-                link="#"
-              />
-              <Card title="HC & Training Series" imgSrc="/hts.jpeg" link="#" />
-              <Card title="Finance Series" imgSrc="/fs.jpeg" link="#" />
-              <Card
-                title="Office & Administration Series"
-                imgSrc="/oas.jpeg"
-                link="#"
-              />
-              <Card
-                title="Leadership & Managerial Series"
-                imgSrc="/lms.jpeg"
-                link="#"
-              />
-              <div className="hidden lg:block"></div>{" "}
-              <Card title="Other Series" imgSrc="/os.jpeg" link="#" />
-            </div>
+        <section className="py-10 px-20">
+          <div className="flex flex-wrap justify-center gap-8">
+            {videos.map((video, index) => (
+              <div
+                key={index}
+                className="relative w-80 h-48 rounded-lg overflow-hidden shadow-lg flex-none"
+              >
+                <Image
+                  src={video.src}
+                  alt={video.title}
+                  layout="fill"
+                  objectFit="cover"
+                  className="transition-transform duration-300 transform"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-20 flex items-end p-4">
+                  <p className="text-white text-lg font-semibold text-left">
+                    {video.title}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
         </section>
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
