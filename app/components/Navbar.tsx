912b965df322ae24f5f46cc9e7d65f849a5031cd
WilliamDevin23 <wdevinsp@gmail.com>
2024-11-18 12:35:22 +0700

    Pointer cursor on hamburger

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar = () => {
 			p-6 border-b-2 w-full bg-neutral-50 top-0 max-h-20 z-50"
     >
       <Logo />
-			<div className="w-8 h-8 space-y-1 flex flex-col items-center justify-center lg:hidden" onClick={setActive}>
+			<div className="cursor-pointer w-8 h-8 space-y-1 flex flex-col items-center justify-center lg:hidden" onClick={setActive}>
 				<div 
 					className={`w-[90%] rounded-lg p-0.5 bg-[#1C75BC] transform transition-transform duration-300 
 						${toggleDropMobile == 1 ? "bg-[#FF0000] translate-y-2 rotate-45":"bg-[#1C75BC] translate-y-0 rotate-0"}`}>
