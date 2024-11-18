ad62a362991f8b3a0877fc95f80783172ffcfd00
WilliamDevin23 <wdevinsp@gmail.com>
2024-11-18 12:34:17 +0700

    Add optional subtitle field

--- app/components/CoverPage.tsx
+++ app/components/CoverPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CoverPage = ({ bgSrc, coverTitle }) => {
+const CoverPage = ({ bgSrc, coverTitle, subTitle }) => {
   return (
 		<div className="relative w-full bg-right-top bg-contain bg-no-repeat z-0"
 			style = {{ 
@@ -10,6 +10,7 @@ const CoverPage = ({ bgSrc, coverTitle }) => {
 			<div className="flex w-[65%] justify-around items-center">
 				<div className="absolute min-[320px]:max-sm:text-[15px] sm:text-lg md:text-4xl lg:text-6xl p-5 max-w-[60%]">
 					<strong>{coverTitle}</strong>
+					<p><strong className="sm:text-md md:text-3xl lg:text-5xl">{subTitle}</strong></p>
 				</div>
 				<svg xmlns="http://www.w3.org/2000/svg"
 					 viewBox="0 0 1050 450">
