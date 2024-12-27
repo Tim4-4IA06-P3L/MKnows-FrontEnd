cdf83c55857b7f850795a80ff412edb8d79dddda
Maula <maulanajmipn@gmail.com>
2024-12-27 15:57:08 +0700

    Backend for submit form Online Training

--- app/what-we-do/training/online-learning/formulir/page.tsx
+++ app/what-we-do/training/online-learning/formulir/page.tsx
@@ -1,9 +1,38 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import Navbar from "../../../../components/Navbar";
 import Footer from "../../../../components/Footer";
 
 const TrainingScheduleForm = () => {
+  const [formData, setFormData] = useState({
+    Name: "",
+    Company: "",
+    Position: "",
+    Email: "",
+    Office_Number: "",
+    WhatsApp_Number: "",
+    Training_Type: "Online",
+  });
+  const sendForm = async (e) => {
+    e.preventDefault();
+    const response = await fetch(
+      "http://localhost:1337/api/training-requests",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          data: formData,
+        }),
+      }
+    );
+    if (response.ok) {
+      window.location.reload();
+    }
+  };
+
   return (
     <div>
       <Navbar />
@@ -31,12 +60,17 @@ const TrainingScheduleForm = () => {
               Formulir Permintaan Jadwal Pelatihan Umum 2024
             </h2>
 
-            <form className="space-y-4">
+            <form className="space-y-4" onSubmit={sendForm}>
               <div>
                 <label className="block text-sm font-medium text-gray-700 mb-1">
                   Nama Lengkap <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.Name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, Name: e.target.value })
+                  }
+                  required
                   type="text"
                   placeholder="Masukkan Nama Lengkap"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -48,6 +82,11 @@ const TrainingScheduleForm = () => {
                   Nama Perusahaan <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.Company}
+                  onChange={(e) =>
+                    setFormData({ ...formData, Company: e.target.value })
+                  }
+                  required
                   type="text"
                   placeholder="Masukkan Nama Perusahaan"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -59,6 +98,11 @@ const TrainingScheduleForm = () => {
                   Jabatan <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.Position}
+                  onChange={(e) =>
+                    setFormData({ ...formData, Position: e.target.value })
+                  }
+                  required
                   type="text"
                   placeholder="Masukkan Jabatan"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -70,6 +114,14 @@ const TrainingScheduleForm = () => {
                   Telepon Kantor <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.Office_Number}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      Office_Number: e.target.value,
+                    })
+                  }
+                  required
                   type="tel"
                   placeholder="Masukkan Nomor WhatsApp"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -81,6 +133,14 @@ const TrainingScheduleForm = () => {
                   Nomor WhatsApp <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.WhatsApp_Number}
+                  onChange={(e) =>
+                    setFormData({
+                      ...formData,
+                      WhatsApp_Number: e.target.value,
+                    })
+                  }
+                  required
                   type="tel"
                   placeholder="Masukkan Nomor WhatsApp"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
@@ -92,6 +152,11 @@ const TrainingScheduleForm = () => {
                   Email <span className="text-red-500">*</span>
                 </label>
                 <input
+                  value={formData.Email}
+                  onChange={(e) =>
+                    setFormData({ ...formData, Email: e.target.value })
+                  }
+                  required
                   type="email"
                   placeholder="Masukkan Email"
                   className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-green-500"
