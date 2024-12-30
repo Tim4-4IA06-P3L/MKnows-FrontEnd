4ede2e56bf9620f9bcad842bb5a31f054ee521eb
Maula <maulanajmipn@gmail.com>
2024-12-30 16:28:43 +0700

    Add page for Pdf

---
+++ app/bootcamp/[id]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-screen h-screen">
+      <iframe
+        src="https://res.cloudinary.com/mknows-cms-upload/image/upload/v1735548932/ACT_1_WILLIAM_DEVIN_SEPTIANUS_PRANGGONO_51421517_487e668699.pdf"
+        width="100%"
+        height="100%"
+      />
+    </div>
+  );
+};
+
+export default page;
