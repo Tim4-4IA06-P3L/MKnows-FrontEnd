64623dda00632cbcf62ad0c55f47d026be074f27
WilliamDevin23 <wdevinsp@gmail.com>
2024-12-31 15:17:57 +0700

    Modify some margins

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 			</section>
 
 			{/* Teks */}
-			<section className="pb-20 pt-14 pl-2 pr-2">
+			<section className="px-8">
 				<div className="container mx-auto text-center">
 					<div className="flex justify-center space-x-3 items-center mb-12">
 						<div className="flex-grow flex-shrink basis-0 text-center">
@@ -50,69 +50,71 @@ export default function Home() {
 							</p>
 						</div>
 					</div>
-					<h2 className="text-3xl font-medium mt-24 -mb-10">
-						Jelajahi Kategori Unggulan
-					</h2>
 				</div>
 			</section>
 
 			{/* Grid */}
-			<section className="flex flex-wrap justify-center gap-8 px-8">
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Program Pelatihan"
-						imgSrc="/ProgPelatihan.jpeg"
-						link=""
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Pembelajaran online, publik & pelatihan internal"
-						imgSrc="/OnlineLearn.png"
-						link="/what-we-do/training"
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Layanan Konsultasi"
-						imgSrc="/consulting-service/Consulting_img.jpg"
-						link="/what-we-do/consulting-service"
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Penilaian Kompetensi Teknis (PKT)"
-						imgSrc="/PKT.png"
-						link="/what-we-do/atc"
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Pelatihan Eksekutif & Pendampingan Bisnis"
-						imgSrc="/PelatihanEkse.jpeg"
-						link="/what-we-do/executive-coaching-and-mentoring"
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Sistem Manajemen Pembelajaran"
-						imgSrc="/SistemBelajar.png"
-						link=""
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Outbound & Gathering"
-						imgSrc="/Outbound.png"
-						link="/what-we-do/outbound"
-					/>
-				</div>
-				<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
-					<Card
-						title="Program kilat pengembangan petugas hybrid untuk industri keuangan"
-						imgSrc="/Kilat.jpeg"
-						link="/what-we-do/officer-development"
-					/>
+			<section className="flex flex-col justify-center px-8 gap-5">
+				<h2 className="text-3xl font-medium text-center">
+					Jelajahi Kategori Unggulan
+				</h2>
+				<div className="flex flex-wrap justify-center gap-8">
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Program Pelatihan"
+							imgSrc="/ProgPelatihan.jpeg"
+							link="/bootcamp"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Pembelajaran online, publik & pelatihan internal"
+							imgSrc="/OnlineLearn.png"
+							link="/what-we-do/training"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Layanan Konsultasi"
+							imgSrc="/consulting-service/Consulting_img.jpg"
+							link="/what-we-do/consulting-service"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Penilaian Kompetensi Teknis (PKT)"
+							imgSrc="/PKT.png"
+							link="/what-we-do/atc"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Pelatihan Eksekutif & Pendampingan Bisnis"
+							imgSrc="/PelatihanEkse.jpeg"
+							link="/what-we-do/executive-coaching-and-mentoring"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Sistem Manajemen Pembelajaran"
+							imgSrc="/SistemBelajar.png"
+							link=""
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Outbound & Gathering"
+							imgSrc="/Outbound.png"
+							link="/what-we-do/outbound"
+						/>
+					</div>
+					<div className="basis-[100%] md:basis-[33%] lg:basis-[25%]">
+						<Card
+							title="Program kilat pengembangan petugas hybrid untuk industri keuangan"
+							imgSrc="/Kilat.jpeg"
+							link="/what-we-do/officer-development"
+						/>
+					</div>
 				</div>
 			</section>
 		</main>
